a4715b58fac75448e635cefec3fc7ea8df2e517c
AussieKing <126050763+AussieKing@users.noreply.github.com>
2023-04-08 13:08:27 +1000

    pushing modified files to include a question file and new classes

---
+++ js/questions.js
@@ -0,0 +1,56 @@
+// creating an array to pass number, questions, options and answers
+let questions = [
+    {
+        // this is the index[0] for the script.js
+        numb : 1,
+        question: "Commonly used data types DO NOT include:",
+        answer: "Alerts",  // this is the correct answer to log later
+        options: [ 
+            "Strings",
+            "Booleans",
+            "Alerts",
+            "Numbers",
+        ]
+    },
+   {numb : 2,
+        question: "The condition in an if/else statement is enclosed within _____.",
+        answer: "parenthesis",  // this is the correct answer to log later
+        options: [ 
+            "quotes",
+            "curly brackets",
+            "parenthesis",
+            "square barckets",
+        ]
+   },
+   {numb : 3,
+    question: "Arrays in JavaScrips can be used to store _____",
+    answer: "all of the above",  // this is the correct answer to log later
+    options: [ 
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above",
+    ]
+},
+{numb : 4,
+    question: "String values must be enclosed within _____ when assigned to variables.",
+    answer: "curly brackets", // this is the correct answer to log later
+    options: [ 
+        "commas",
+        "curly brackets",
+        "quotes",
+        "parenthesis",
+    ]
+},
+{numb : 5,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is ____",
+    answer: "console log",  // this is the correct answer to log later
+    options: [ 
+        "JavaScript",
+        "terminal / bash",
+        "for loops",
+        "console log",
+    ]
+},
+
+];
